5478e0039b9197af8e8e86df9aaa31d9df90b819
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-07-18 13:43:37 -0400

    Time: 0 ms (100.00%), Space: 40.4 MB (26.02%) - LeetHub

---
+++ linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,85 @@
+/**
+ * Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+ */
+// class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode(int x) {
+//         val = x;
+//         next = null;
+//     }
+// }
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (true){
+            if(fast.next == null){
+                return false;
+            }
+            slow = slow.next;
+            if(fast.next.next == null)
+                return false;
+            else
+                fast = fast.next.next;
+            if( slow == fast) {
+                return true;
+            }
+
+        }
+        // return false;
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     public boolean hasCycle(ListNode head) {
+//         ListNode slow = head;//0
+//         ListNode fast = head;//0
+//         while(true){
+//             if (head == null) return false;
+//             if(fast.next != null) 
+//                 fast = fast.next;//1 3 5
+//             else 
+//                 return false;
+            
+//             if (fast == slow ) //1, 0 | 3, 1 | 5 2|
+//                 return true;
+            
+
+//             if(slow.next != null) 
+//                 slow = slow.next; //1 2
+//             else 
+//                 return false;
+            
+//             if(fast.next != null) {
+//                 fast = fast.next; //2 4
+//             }
+//             else 
+//                 return false;
+
+//         }
+        
+//     }
+// }
