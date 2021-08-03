2ce04cd50bf60842d0212967ba450d8b0721b7cd
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-08-03 08:23:57 -0400

    Time: 0 ms (100.00%), Space: 40.4 MB (23.27%) - LeetHub

--- linked-list-cycle/linked-list-cycle.java
+++ linked-list-cycle/linked-list-cycle.java
@@ -9,35 +9,36 @@ class ListNode {
     }
 }
  */
-// class ListNode {
-//     int val;
-//     ListNode next;
-//     ListNode(int x) {
-//         val = x;
-//         next = null;
-//     }
-// }
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if (head == null) return false;
         ListNode slow = head;
         ListNode fast = head;
         
-        while (true){
-            if(fast.next == null){
-                return false;
-            }
+        while (fast != null && fast.next != null){
             slow = slow.next;
-            if(fast.next.next == null)
-                return false;
-            else
-                fast = fast.next.next;
-            if( slow == fast) {
-                return true;
-            }
-
+            fast = fast.next.next;
+            
+            if (fast == slow) return true;
         }
-        // return false;
+        
+        
+        // while (true){
+            // if(fast.next == null){
+            //     return false;
+            // }
+            // slow = slow.next;
+            // if(fast.next.next == null)
+            //     return false;
+            // else
+            //     fast = fast.next.next;
+            // if( slow == fast) {
+            //     return true;
+            // }
+
+        // }
+        //reached end of list
+        return false;
     }
 }
     
