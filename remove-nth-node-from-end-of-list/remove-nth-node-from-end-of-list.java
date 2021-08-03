466349e85835f55e88772c15ee370c535972e0c6
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-08-03 10:47:42 -0400

    Time: 0 ms (100.00%), Space: 36.9 MB (63.77%) - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -8,8 +8,70 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         // if head is null
+//         if (head == null) return null;
+//         if (head.next == null && n == 1) return null;
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         int slowIndex = 1, fastIndex = 1, prev = -1;
+//         while(slow != null ){
+
+//             if(fast!= null && fast.next!=null ) {
+//                 fast = fast.next;
+//                 fastIndex++;
+//                 // System.out.println("fastIndex "+fastIndex);
+//             }
+//             else {
+//                 if (prev == -1){
+//                 //first occurance where fast == null
+//                 // set the prev node value here
+//                     prev = fastIndex - n;
+//                     // System.out.println(prev);
+//                 }
+//             }
+            
+            
+//             if(fast!= null && fast.next!=null ) {
+//                 fast = fast.next;
+//                 fastIndex++;
+//                 // System.out.println("fastIndex "+fastIndex);
+//             }
+//             else {
+//                 if (prev == -1){
+//                 //first occurance where fast == null
+//                 // set the prev node value here
+//                     prev = fastIndex - n;
+//                     // System.out.println(prev);
+
+//                 }
+//             }
+//             if(slowIndex == prev){
+//                 // this is the prev element of the element to be removed.
+//                 // remove element and return head. 
+//                 slow.next = slow.next.next;
+//                 return head;
+//             }
+//             if (slow != null && slow.next != null){
+//                 slow = slow.next;
+//                 slowIndex++;
+//                 // System.out.println("slowIndex "+slowIndex);
+
+//             }
+
+
+//         }
+//         // We should not reach here
+//         return head;
+//     }
+// }
+// Below is not a single pass solution.
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        // if head is null
+        if (head == null) return null;
+        if (head.next == null && n == 1) return null;
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode first = head;
