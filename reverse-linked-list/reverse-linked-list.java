cde721af9afa2e9a11328165b03101107547a40b
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-08-04 19:13:40 -0400

    Time: 0 ms (100.00%), Space: 38.9 MB (47.12%) - LeetHub

--- reverse-linked-list/reverse-linked-list.java
+++ reverse-linked-list/reverse-linked-list.java
@@ -8,27 +8,54 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode reverseList(ListNode head) {
-        
-        //temp to set head pointer to null.
-        ListNode temp = head;
-
-        //The reverse list
+        //handle the edge cases
+        if(head == null) return null;
+        //construct the revList using below pointer
         ListNode revList = null;
+        return helper(head, revList);
+    }
+    
+    public ListNode helper(ListNode head, ListNode revList){
+        //if reached end of list, return the reversed list
+        if(head == null) return revList;
+        //get the reference of next node in list
+        ListNode temp = head.next;
+
+        //assign present node as head of revList
+        head.next = revList;
+        revList = head;
         
-        while (temp != null ){
-            //get the next node in temporary node
-            ListNode itr = temp.next;
-
-            //set the next pointer to revList
-            temp.next = revList;
-            
-            //set the itr pointer as new reverse list 
-            revList = temp;
-
-            temp = itr;
-        }
-        return revList;
+        //proceed recursively until end of list
+        return helper(temp, revList);
+        
     }
 }
+
+/***Below is iterative solution***/
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+        
+//         //temp to set head pointer to null.
+//         ListNode temp = head;
+
+//         //The reverse list
+//         ListNode revList = null;
+        
+//         while (temp != null ){
+//             //get the next node in temporary node
+//             ListNode itr = temp.next;
+
+//             //set the next pointer to revList
+//             temp.next = revList;
+            
+//             //set the itr pointer as new reverse list 
+//             revList = temp;
+
+//             temp = itr;
+//         }
+//         return revList;
+//     }
+// }
