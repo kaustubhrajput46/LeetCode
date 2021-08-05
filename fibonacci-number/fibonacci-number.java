159013fe00f27a4e7248cbc55fd7b5ca60405109
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-08-04 20:35:34 -0400

    Time: 0 ms (100.00%), Space: 35.8 MB (54.71%) - LeetHub

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -1,8 +1,40 @@
 class Solution {
-    public int fib(int n) {
-        if (n == 0 ) return 0;
-        if (n == 1 ) return 1;
+    
+//   HashMap<Integer, Integer> cache = new HashMap<Integer, Integer>();
+//     public int fib(int n) {
+//         if (cache.containsKey(n)) {
+//           return cache.get(n);
+//         }
         
-        return fib(n-1) + fib(n-2); 
+//         if (n == 0 ) return 0;
+//         if (n == 1 ) return 1;
+//         int result = fib(n-1) + fib(n-2);
+        
+//         cache.put(n, result);
+//         return result;
+//     }
+    
+    
+    
+    int[] cache;
+    public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        cache = new int[n+1];
+        // cache[1] = 1;
+        return helper(n);
     }
+    
+    public int helper(int n){
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (cache[n] != 0) return cache[n];
+        
+        int res = helper(n-1) + helper(n-2);
+        
+        cache[n] = res; 
+        return res;
+        
+    }
+
 }
