8a875716feb23da41be18d4a4c162f12dbf92d73
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-08-11 16:11:15 -0400

    Time: 2 ms (77.31%), Space: 39.1 MB (58.79%) - LeetHub

---
+++ valid-sudoku/valid-sudoku.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        char dot = '.';
+        Set<Integer> set = new HashSet<>();
+        //check rows
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == dot) continue;
+                if(set.contains(board[i][j] - '0')) return false;
+                else set.add(board[i][j] - '0');
+            }
+            set.clear();
+        }
+        // set.clear();
+        // System.out.println("row clear");
+        //check columns
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[j][i] == dot) continue;
+                // int num = board[i][j] - '0';
+                // System.out.println(num);
+                // int num = Integer.parseInt(board[i][j]);
+                if(set.contains(board[j][i] - '0')) return false;
+                else set.add(board[j][i] - '0');
+            }
+            set.clear();
+        }
+        // set.clear();
+        // System.out.println("column clear");
+        for(int k = 0; k < 9; k=k+3){
+            set.clear();
+            for(int i = 0; i < 9; i++){
+                for(int j = k; j < k+3; j++){
+                    if(board[i][j] == dot) continue;
+                    if(set.contains(board[i][j] - '0')) return false;
+                    else set.add(board[i][j] - '0');        
+                }
+                //clear set after every 1 interations
+                if(i == 2 || i == 5) set.clear();
+            }
+        }
+
+        
+        return true;
+    }
+}
