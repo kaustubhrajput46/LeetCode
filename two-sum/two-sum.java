afc8e2288f9c890b220b08dfcecbde3c7982b7bc
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-08-28 11:55:52 -0400

    Time: 1 ms (99.58%), Space: 39.2 MB (63.10%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,15 +1,24 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for(int i = 0; i< nums.length; i++){
+//             if(i !=map.getOrDefault(target - nums[i], i))
+//                 return new int[]{i, map.get(target - nums[i])};
+//             map.put(nums[i], i);
+//         }
+//         return new int[]{};
+//     }
+// }
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i< nums.length; i++){
-            // if(nums[i] <= target){
-            //     System.out.println(i);
-                if(map.containsKey(target - nums[i])) 
-                    if(i !=map.get(target - nums[i])) {
-                        System.out.println("Hey");
-                        return new int[]{i, map.get(target - nums[i])};
-                    }
-            // }
+            if(map.containsKey(target - nums[i])) 
+                if(i !=map.get(target - nums[i])) {
+                    return new int[]{i, map.get(target - nums[i])};
+                }
             map.put(nums[i], i);
         }
         return new int[]{};
