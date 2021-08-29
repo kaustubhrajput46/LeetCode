99b2cfde05f12419f2294f3cda26588d6ff21921
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-08-29 11:25:02 -0400

    Time: 2 ms (62.52%), Space: 52.1 MB (67.39%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -5,21 +5,14 @@ class Solution {
         
         for(int i = 0; i < len - 1; i++){
             if(prices[i+1] <= prices[i]) continue;
-            
-            
             for (int j = i+1; j < len; j++){
-                // System.out.println(i+" "+max);
                 if(prices[j] <= prices[i] && j<len-1) {
-                System.out.println(i+" "+j+" "+max);
                     i=j;
-                    // break;
                 }
                 else
-                
-                if(prices[j] - prices[i] > max){
-                    max = prices[j] - prices[i];
-                }
-                // System.out.println(i+" "+j+" "+max);
+                    if(prices[j] - prices[i] > max){
+                        max = prices[j] - prices[i];
+                    }
             }
         }
         return max;
