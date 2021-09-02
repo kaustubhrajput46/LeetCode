8d6392fc5a3a75cbe04cc551a4c4476952df4ab2
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-09-01 20:06:24 -0400

    Time: 13 ms (66.65%), Space: 45.3 MB (15.94%) - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,21 +1,34 @@
-class Solution {
+public class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
-        int n = s.length();
-        // build hash map : character and how often it appears
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            count.put(c, count.getOrDefault(c, 0) + 1);
-        }
-        
-        // find the index
-        for (int i = 0; i < n; i++) {
-            if (count.get(s.charAt(i)) == 1) 
+        int freq [] = new int[26];
+        for(int i = 0; i < s.length(); i ++)
+            freq [s.charAt(i) - 'a'] ++;
+        for(int i = 0; i < s.length(); i ++)
+            if(freq [s.charAt(i) - 'a'] == 1)
                 return i;
-        }
         return -1;
     }
 }
+
+// class Solution {
+//     public int firstUniqChar(String s) {
+//         HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+//         int n = s.length();
+//         // build hash map : character and how often it appears
+//         for (int i = 0; i < n; i++) {
+//             char c = s.charAt(i);
+//             count.put(c, count.getOrDefault(c, 0) + 1);
+//         }
+        
+//         // find the index
+//         for (int i = 0; i < n; i++) {
+//             if (count.get(s.charAt(i)) == 1) 
+//                 return i;
+//         }
+//         return -1;
+//     }
+// }
+
 // class Solution {
 //     public int firstUniqChar(String s) {
 //         if (s.length() == 1) return 0;
