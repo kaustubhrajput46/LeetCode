25f124a96195223fd4fa71bef9901b77a24a3858
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-09-03 09:18:40 -0400

    Time: 41 ms (5.02%), Space: 48.8 MB (6.40%) - LeetHub

---
+++ ransom-note/ransom-note.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        //construct hashmap for magazine
+        for(int i = 0; i < magazine.length(); i++){
+            if(map.containsKey(magazine.charAt(i))){
+                map.put(magazine.charAt(i), map.get(magazine.charAt(i))+1);
+            } else {
+                map.put(magazine.charAt(i), 1);
+            }
+        }
+        
+        //tranverse ransomNote
+        for(int i = 0; i < ransomNote.length(); i++){
+            if(map.containsKey(ransomNote.charAt(i))){
+                if(map.get(ransomNote.charAt(i)) == 1){
+                    map.remove(ransomNote.charAt(i));
+                } else {
+                    map.put(ransomNote.charAt(i), map.get(ransomNote.charAt(i))-1);
+                }
+            } else 
+                return false;
+        }
+        
+        
+        return true;
+    }
+}
