61130e1edf22635ac7d83cbf8127316aaa9cd9b1
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-09-03 15:01:48 -0400

    Time: 42 ms (17.78%), Space: 116.3 MB (5.03%) - LeetHub

---
+++ contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (k == 0) return false;
+        
+        Map<Integer,  Integer> map = new HashMap<>();
+        
+        int res = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                if(i - map.get(nums[i]) <= k) return true;
+                map.put(nums[i], i);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        
+        return false;
+    }
+}
