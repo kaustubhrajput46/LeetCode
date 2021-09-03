c06283b4168f84c72c3c25cd504a9719941127c9
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-09-03 08:54:06 -0400

    Time: 7 ms (5.35%), Space: 44.6 MB (6.70%) - LeetHub

--- linked-list-cycle/linked-list-cycle.java
+++ linked-list-cycle/linked-list-cycle.java
@@ -1,86 +1,13 @@
-/**
- * Definition for singly-linked list.
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
-        val = x;
-        next = null;
-    }
-}
- */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null) return false;
-        ListNode slow = head;
-        ListNode fast = head;
-        
-        while (fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-            
-            if (fast == slow) return true;
+        Set<ListNode> nodesSeen = new HashSet<>();
+        while (head != null) {
+            if (nodesSeen.contains(head)) {
+                return true;
+            }
+            nodesSeen.add(head);
+            head = head.next;
         }
-        
-        
-        // while (true){
-            // if(fast.next == null){
-            //     return false;
-            // }
-            // slow = slow.next;
-            // if(fast.next.next == null)
-            //     return false;
-            // else
-            //     fast = fast.next.next;
-            // if( slow == fast) {
-            //     return true;
-            // }
-
-        // }
-        //reached end of list
         return false;
     }
 }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//     public boolean hasCycle(ListNode head) {
-//         ListNode slow = head;//0
-//         ListNode fast = head;//0
-//         while(true){
-//             if (head == null) return false;
-//             if(fast.next != null) 
-//                 fast = fast.next;//1 3 5
-//             else 
-//                 return false;
-            
-//             if (fast == slow ) //1, 0 | 3, 1 | 5 2|
-//                 return true;
-            
-
-//             if(slow.next != null) 
-//                 slow = slow.next; //1 2
-//             else 
-//                 return false;
-            
-//             if(fast.next != null) {
-//                 fast = fast.next; //2 4
-//             }
-//             else 
-//                 return false;
-
-//         }
-        
-//     }
-// }
