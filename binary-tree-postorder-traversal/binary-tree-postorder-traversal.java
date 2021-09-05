0951bdea6986dce7e9988e0051cc69c63f763236
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-09-05 03:01:11 -0400

    Time: 0 ms (100.00%), Space: 37.3 MB (54.20%) - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -13,41 +13,20 @@
  *     }
  * }
  */
+
 class Solution {
-     public List<Integer> postorderTraversal(TreeNode root){
-        List<Integer> list = new LinkedList<>();
-        if(root == null) return list;
-        //CLR => CRL => LRC
-        Stack<TreeNode> stack1 = new Stack<>();
-        Stack<TreeNode> stack2 = new Stack<>();
-        stack1.push(root);
-        while(!stack1.isEmpty()){
-            TreeNode pop = stack1.pop();
-            stack2.push(pop);
-            if(pop.left != null) stack1.push(pop.left);
-            if(pop.right != null) stack1.push(pop.right);
-        }
-        
-        while(!stack2.isEmpty()){
-            list.add(stack2.pop().val);
-        }
-        
-        return list;
+    List<Integer> in;
+    public List<Integer> postorderTraversal(TreeNode root) {
+        in = new ArrayList<Integer>();
+        helper(root);
+        return in;    
+    }
+    
+    public void helper(TreeNode root){
+        if (root == null) return;        
+        helper(root.left);
+        helper(root.right);
+        in.add(root.val);
     }
 }
-// class Solution {
-//     List<Integer> in;
-//     public List<Integer> postorderTraversal(TreeNode root) {
-//         in = new ArrayList<Integer>();
-//         helper(root);
-//         return in;    
-//     }
-    
-//     public void helper(TreeNode root){
-//         if (root == null) return;        
-//         helper(root.left);
-//         helper(root.right);
-//         in.add(root.val);
-//     }
-// }
 
