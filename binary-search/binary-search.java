0237d66ed7a714ce76f1346222f70b13412c32e5
KAUSTUBH RAJPUT <kaustubhrajput46@gmail.com>
2021-09-30 11:58:48 -0400

    Time: 0 ms (100.00%), Space: 40 MB (64.03%) - LeetHub

--- binary-search/binary-search.java
+++ binary-search/binary-search.java
@@ -1,18 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int len = nums.length;
-        int mid = len/2;
+        if (nums == null || nums.length == 0) return -1;
+        int end = nums.length -1;
+        int pivot = 0;
         int start = 0;
-        int end = len -1;
         while(start <= end){
-            System.out.println("start: "+start+" mid: "+mid+" end: "+end);
-            if(nums[mid] == target) return mid;
-            if(target > nums[mid]){
-                start = mid+1;
-                mid = (end + start)/2;
+            // System.out.println("start: "+start+" mid: "+mid+" end: "+end);
+            pivot = (end + start)/2;
+            if(nums[pivot] == target) return pivot;
+            if(target > nums[pivot]){
+                start = pivot+1;
             } else {
-                end = mid-1;
-                mid = (end + start)/2;
+                end = pivot-1;
             }
         }
         
