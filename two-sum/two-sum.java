9ad3a08a97984dd0830df67aad7c7a85b7ab7326
Kaustubh Rajput <kaustubhrajput46@gmail.com>
2021-12-28 11:21:40 -0500

    Time: 1 ms (99.71%), Space: 38.9 MB (86.36%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,3 +1,72 @@
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        int len = nums.length;
+        
+        for(int i = 0; i < len; i++){
+            if(map.containsKey(target - nums[i]))
+                return new int[]{map.get(target - nums[i]), i};
+            else
+                map.put(nums[i], i);
+            
+        }
+        
+        
+        // for(int i : nums){
+        //     if(map.contains(target - i))
+        //         return new int[]{i, target - i};
+        //     else
+        //         map.add(i);
+        // }
+        return new int[]{0,0};
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // class Solution {
 //     public int[] twoSum(int[] nums, int target) {
 //         Map<Integer, Integer> map = new HashMap<>();
@@ -11,16 +80,16 @@
 // }
 
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i< nums.length; i++){
-            if(map.containsKey(target - nums[i])) 
-                if(i !=map.get(target - nums[i])) {
-                    return new int[]{i, map.get(target - nums[i])};
-                }
-            map.put(nums[i], i);
-        }
-        return new int[]{};
-    }
-}
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for(int i = 0; i< nums.length; i++){
+//             if(map.containsKey(target - nums[i])) 
+//                 if(i !=map.get(target - nums[i])) {
+//                     return new int[]{i, map.get(target - nums[i])};
+//                 }
+//             map.put(nums[i], i);
+//         }
+//         return new int[]{};
+//     }
+// }
